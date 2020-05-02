9b2b91e40ce73cc3c521c6e320604eb87c780828
Priyankadubey190 <priyankadubey0811@gmail.com>
2020-05-02 09:44:25 -0700

    first commit

---
+++ mynewapp/src/myfile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class myfile extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default myfile;
